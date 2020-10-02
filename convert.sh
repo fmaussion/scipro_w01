4e3810168bef717520c082b1bc88a8b8e4985e9c
Fabien Maussion <fabien.maussion@uibk.ac.at>
2020-10-02 21:05:40 +0200

    new year

--- convert.sh
+++ convert.sh
@@ -3,4 +3,6 @@
 jupyter-nbconvert --to slides slides.ipynb --reveal-prefix=reveal.js
 mv slides.slides.html  index.html
 sed -i -e 's/slides slides/Scientific programming/g' index.html
+sed -i -e 's/transition: "slide"/transition: "none"/g' index.html
+
 
